43ccf1b58414431ca2668eb0a93751eb79acb969
Mike Sallese <msallese31@gmail.com>
2016-07-17 14:26:25 -0400

    have view return array of times so nick can handle

--- POC/django_backend/chrome/video_process/views.py
+++ POC/django_backend/chrome/video_process/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world")
+    listToReturn = json.dumps({"test_list_of_times" : ["1:31", "0:12"]})
+    return HttpResponse(listToReturn, content_type="application/json")
