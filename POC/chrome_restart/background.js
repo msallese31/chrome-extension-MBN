551f0954126fb140ac39c0b402aa1a011f6bc846
Nicholas Qiao <qiao.nicholas@gmail.com>
2016-07-23 18:50:35 -0400

    bg stuff

--- POC/chrome_restart/background.js
+++ POC/chrome_restart/background.js
@@ -1,20 +1,29 @@
+var counter = 0;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(changeInfo && changeInfo.status == "complete"){
         //console.log("Tab updated: " + tab.url);
-
-        chrome.tabs.sendMessage(tabId, {data: tab}, function(response) {
-            //console.log(response);
+        console.log("bg.js - url changed, sending message to content.js");
+        chrome.tabs.sendMessage(tabId, {data: tab, greeting: "urlChanged"}, function(response) {
+            
         });
 
     }
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.type == "notification")
-      say();
+//Listener
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (counter == 0) {
+    if (request.greeting == "YouTubeValid") {
+      sendAJAX();
+      sendResponse({farewell: "bg.js - received youtube is valid"});
+      
+    }
+  }
+  counter = (counter+1)%2;
 });
 
-function say() {
+function sendAJAX() {
   console.log("got mesg from content");
   chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -26,9 +35,10 @@ function say() {
     chrome.tabs.executeScript(null, {file: "content.js"});
     sendResponse({farewell: "in backgorund.js"});
     console.log("whats going on");
-//document.body.style.backgroundColor="red";
 
   var URL = 'https://www.youtube.com/watch?v=K1Y6PchDYfw'
+  
+  //var URL = 'https://www.youtube.com/watch?v=lFIIMEe2Ht0'
   var searchUrl = `http://127.0.0.1:8000/video_process/checkIndex?link=${URL}`;
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
@@ -36,36 +46,24 @@ function say() {
   x.responseType = 'text';
   x.onload = function() {
     // Parse and process the response from Google Image Search.
-
     var response = x.response;
-/*    document.write("Processing Dictionary ");
-    document.write();
-    document.write("Keys: ");*/
+    console.log(response);
 
-/*    for (var keys in response) {
-      document.write(keys);
-    }*/
+    sendArray(response);
 
-    // var length = response["test_list_of_times"].length;
-
-    // for (var values in response["test_list_of_times"]) {
-      console.log(response);
-      done();
-    // }
-   
-/*    for (var x = 0; x < length; x++) {
-      document.write(response["test_list_of_times"][x]);
-    }
-*/
- 
   };
+
   x.onerror = function() {
     errorCallback('Network error.');
   };
-  x.send();
+    x.send();
   });
 }
 
-function done() {
-  console.log("done!!");
+function sendArray(response) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "responseArray", data: response}, function(response) {
+    console.log(response.farewell);
+  });
+});
 }
